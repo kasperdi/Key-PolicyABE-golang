a8c64eb603c52dc718685d53200dc33fbaa41fd0
kasperdi di <kasperdi1412@gmail.com>
2022-12-08 10:31:17 +0100

    Added code and data for plotting

---
+++ plot.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+var = pd.read_excel("ABE_Setup_Runtime.xlsx")
+
+attributes = list(var['Attributes'])
+timeMs = list(var['Time(ms)'])
+
+plt.figure(figsize=(10,10))
+plt.scatter(timeMs,attributes,marker=".",s=100,c="blue")
+plt.title("ABE Setup runtime")
+plt.xscale('log', basex=2)
+plt.yscale('log')
+plt.ylabel("Attributes (universe size)")
+
+plt.xlabel("Milliseconds")
+plt.show()
+
+var = pd.read_excel("ABE_Keygen_Runtime.xlsx")
+
+NodesReq = list(var['NodesRequired'])
+timeMs = list(var['Time(ms)'])
+
+plt.figure(figsize=(10,10))
+plt.scatter(timeMs,NodesReq,marker=".",s=100,c="blue")
+plt.title("ABE Keygen runtime")
+plt.ylabel("Total # nodes that have to be satisfied")
+plt.xlabel("Milliseconds")
+plt.show()
+
+var = pd.read_excel("ABE_Encryption_Runtime.xlsx")
+
+attributes = list(var['Attributes'])
+timeMs = list(var['Time(ms)'])
+
+plt.figure(figsize=(10,10))
+plt.scatter(timeMs,attributes,marker=".",s=100,c="blue")
+plt.title("ABE Encrypt runtime")
+plt.ylabel("Attributes")
+plt.xlabel("Milliseconds")
+plt.show()
+
+var = pd.read_excel("ABE_Decryption_Runtime.xlsx")
+
+attributes = list(var['NodesRequired'])
+timeMs = list(var['Time(ms)'])
+
+plt.figure(figsize=(10,10))
+plt.scatter(timeMs,attributes,marker=".",s=100,c="blue")
+plt.title("ABE Decrypt runtime")
+plt.ylabel("Total # nodes that have to be satisfied")
+plt.xlabel("Milliseconds")
+plt.show()
